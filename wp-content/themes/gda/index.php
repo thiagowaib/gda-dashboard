a0a9158169c793694bb2df77df9090adeeb50e1f
thiagowaib <thiagowaib@gmail.com>
2025-08-23 23:54:49 -0300

    Basic Map

--- wp-content/themes/gda/index.php
+++ wp-content/themes/gda/index.php
@@ -3,9 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+     integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+     crossorigin=""/>
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+     integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+     crossorigin=""></script>
+
+    <link rel="stylesheet" href="<?= get_stylesheet_directory_uri(); ?>/style.css"/>
+    <script src="<?= get_template_directory_uri(); ?>/script.js" defer></script>
+    <title><?= bloginfo('description'); ?></title>
 </head>
 <body>
-    Hello World
+    <div id="map"></div>
 </body>
 </html>
---
+++ wp-content/themes/gda/script.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([-19.72806, -50.19556], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
